a5d05f1564df73f5db2eff6525379a927df1e106
Michi <80653319+Whiteherobot@users.noreply.github.com>
2022-06-11 13:15:48 -0500

    Correccion de Sistema

--- src/main/java/vistaCliente/VistaFactura.java
+++ src/main/java/vistaCliente/VistaFactura.java
@@ -5,7 +5,6 @@ import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
